e61156f3671a322a503e273565f31c9a113641b7
janesummer <1752321720@qq.com>
2020-01-17 23:02:58 +0800

    新增获取唯一id

--- module/user.js
+++ module/user.js
@@ -2,11 +2,23 @@ const mysqlOpt = require('../util/mysqlOpt');
 const qs = require('qs');
 const msgResult = require('./msgResult');
 const util = require('../util/util');
+const https = require('https');
+
+
 
 
 var login = (req, resp) => {
-  resp.json(msgResult.msg('成功访问'));
-  // var user = qs.parse(req.body);
+  var query = qs.parse(req.body);
+  var code = query.code;
+  var user = query.user;
+  let code2Session;
+  https.get(`https://api.weixin.qq.com/sns/jscode2session?appid=wx2bca6a5670f63aee&secret=cd00a7c8648a2f9de2bbf6fdd130aa35&js_code=${code}&grant_type=authorization_code`, data => {
+    console.log(data)
+    code2Session = data;
+    getId();
+  })
+
+
   // if (!user || !user.name || !user.pwd || !user.rule) {
   //   resp.json(msgResult.error("参数不合法"));
   //   return;
@@ -29,6 +41,24 @@ var login = (req, resp) => {
   //     resp.end(msgResult.error(e.message));
   //   }
   // )
+  function getId () {
+    const WXBizDataCrypt = require('../util/WXBizDataCrypt')
+    var appId = 'wx2bca6a5670f63aee'
+    var sessionKey = code2Session.session_key
+    var encryptedData = user.encryptedData
+    var iv = user.encryptedData.iv
+
+    var pc = new WXBizDataCrypt(appId, sessionKey)
+
+    var data = pc.decryptData(encryptedData , iv)
+
+    console.log('解密后 data: ', data)
+
+    resp.json(msgResult.msg(data));
+  }
+  
+
+
 };
 
 var register = (req, resp) => {
