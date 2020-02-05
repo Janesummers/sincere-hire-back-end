467aeb4faf932394803b42768786b8b423961442
janesummer <1752321720@qq.com>
2020-02-05 22:04:28 +0800

    更新数据库

--- qz.sql
+++ qz.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50728
  File Encoding         : 65001
 
- Date: 04/02/2020 20:57:07
+ Date: 05/02/2020 22:01:52
 */
 
 SET NAMES utf8mb4;
@@ -32,10 +32,11 @@ CREATE TABLE `user` (
   `city` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `identity` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `advantage` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `progress` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `position` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
   `company` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
   `industry` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `campanyScale` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `companyScale` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `progress` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `rule` int(2) DEFAULT NULL,
   PRIMARY KEY (`unionid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -53,6 +54,6 @@ CREATE TABLE `user_education` (
   `time_enrollment` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `time_graduation` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
