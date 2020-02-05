bd44d1a3a61b782a3b41401b4fb4b5ce97c3e6c4
janesummer <1752321720@qq.com>
2020-02-05 21:57:02 +0800

    新增开放数据处理文件

---
+++ module/openData.js
@@ -0,0 +1,40 @@
+const qs = require('qs');
+const path = require('path');
+const fs = require('fs');
+const msgResult = require('./msgResult');
+
+var getCity = (req, resp) => {
+  let unionid = req.query.unionid;
+  if (!unionid || unionid.length != 28) {
+    resp.json(msgResult.error("参数非法"));
+    return;
+  }
+  let data = fs.readFileSync(path.join(__dirname, '../static/city.js'), {encoding: 'utf8'});
+  resp.json(msgResult.msg(JSON.parse(data)));
+}
+
+var getSchool = (req, resp) => {
+  let unionid = req.query.unionid;
+  if (!unionid || unionid.length != 28) {
+    resp.json(msgResult.error("参数非法"));
+    return;
+  }
+  let data = fs.readFileSync(path.join(__dirname, '../static/school.js'), {encoding: 'utf8'});
+  resp.json(msgResult.msg(JSON.parse(data)));
+}
+
+var getMajor = (req, resp) => {
+  let unionid = req.query.unionid;
+  if (!unionid || unionid.length != 28) {
+    resp.json(msgResult.error("参数非法"));
+    return;
+  }
+  let data = fs.readFileSync(path.join(__dirname, '../static/major.js'), {encoding: 'utf8'});
+  resp.json(msgResult.msg(JSON.parse(data)));
+}
+
+module.exports = {
+  getCity,
+  getSchool,
+  getMajor
+}
