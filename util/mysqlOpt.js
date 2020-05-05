b6e30d566f968851914c72f29991829206178aa5
Janesummers <1752321720@qq.com>
2020-05-05 12:08:30 +0800

    删除数据源

--- util/mysqlOpt.js
+++ util/mysqlOpt.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 let pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'janesummer1175',
+  password: 'root',
   port: '3306',
   database: 'qz'
 });
